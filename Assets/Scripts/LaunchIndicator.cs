706569603b57be3fe7f2c7298bc9791df5b96eed
Jordan Truong <jordantruong26@gmail.com>
2025-02-08 20:23:14 -0800

    Launch Indicator

---
+++ Assets/Scripts/LaunchIndicator.cs
@@ -0,0 +1,16 @@
+using System.Runtime.CompilerServices;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class LaunchIndicator : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera freeLookCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.forward = freeLookCamera.transform.forward;
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+
+    }
+}
