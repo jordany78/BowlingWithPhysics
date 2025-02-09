6b32ea29046f2872be3ba1ccd24e061d5aff53e2
Jordan Truong <jordantruong26@gmail.com>
2025-02-08 19:19:13 -0800

    Added gutter prefab
    if ball goes in gutter, the gutter takes over movement control of ball and sends it along the gutter

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,6 +6,7 @@ public class BallController : MonoBehaviour
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
 
+    private bool isBallLaunched;
     private Rigidbody ballRB;
 
     void Start()
@@ -17,6 +18,8 @@ public class BallController : MonoBehaviour
 
     private void LaunchBall()
     {
+        if (isBallLaunched) return;
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 
