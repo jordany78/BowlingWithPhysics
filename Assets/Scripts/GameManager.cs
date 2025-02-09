61e50ce3afaf4d4eaa636606ea0263a4605df57f
Jordan Truong <jordantruong26@gmail.com>
2025-02-08 21:19:24 -0800

    Added Reset functionality

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float score = 0;
+
+    [SerializeField] private BallController ball;
+    [SerializeField] private GameObject pinCollection;
+    [SerializeField] private Transform pinAnchor;
+    [SerializeField] private InputManager inputManager;
+
+
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private FallTrigger[] pins;
+    private GameObject pinObjects;
+
+    private void Start()
+    {
+        inputManager.OnResetPressed.AddListener(HandleReset);
+        SetPins();
+    }
+
+    private void HandleReset()
+    {
+        ball.ResetBall();
+        SetPins();
+    }
+
+    private void SetPins()
+    {
+        if (pinObjects)
+        {
+            foreach(Transform child in pinObjects.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            Destroy(pinObjects);
+        }
+        pinObjects = Instantiate(pinCollection, pinAnchor.transform.position, Quaternion.identity, transform);
+        pins = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+        foreach(FallTrigger pin in pins)
+        {
+            pin.OnPinFall.AddListener(IncrementScore);
+        }
+    }
+
+    private void IncrementScore()
+    {
+        score++;
+        scoreText.text = $"Score: {score}";
+    }
+}
